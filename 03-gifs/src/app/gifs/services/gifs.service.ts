48b922a860f5c7b6464dbb9318314e31ad615901
andoni980 <andonilarrazabal980@gmail.com>
2024-10-03 14:06:26 +0200

    http consulta api

--- 03-gifs/src/app/gifs/services/gifs.service.ts
+++ 03-gifs/src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchReponse } from '../interfaces/gifs.interfaces'
+
+// const GIPHY_API_KEY = 'WvUn69D7HMmqtgNtRqJewSspQNdKGIvr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+  public gifsList: Gif[] = [];
 
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'WvUn69D7HMmqtgNtRqJewSspQNdKGIvr';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -29,6 +36,30 @@ export class GifsService {
       return;
     }
     this.organizeHistory(tag);
-  }
 
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag)
+
+    this.http.get<SearchReponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        this.gifsList = resp.data;
+        console.log({ gifs: this.gifsList });
+
+      })
+
+
+
+
+
+
+    // fetch('http://api.giphy.com/v1/gifs/search?api_key=WvUn69D7HMmqtgNtRqJewSspQNdKGIvr&q=Valorant&limit=10')
+    //   .then(resp => resp.json())
+    //   .then(data => console.log(data));
+
+    // const resp = await fetch('http://api.giphy.com/v1/gifs/search?api_key=WvUn69D7HMmqtgNtRqJewSspQNdKGIvr&q=Valorant&limit=10')
+    // const data = await resp.json();
+    // console.log(data);
+  }
 }
